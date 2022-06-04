827be2bbb5f1393baa56070eaebf85ea86cc05c7
AymanRefat <84641883+AymanRefat@users.noreply.github.com>
2022-06-04 14:59:33 +0200

    Add Messages in Order Behaviors ( Create - Update - Delete )

--- orders/views.py
+++ orders/views.py
@@ -6,18 +6,20 @@ from django.views.generic import (
 		ListView,
 		UpdateView,
 )
-from .models import Order 
-
+from django.http import HttpResponse
+from .models import Order
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from typing import Optional
 from django.forms.models import modelform_factory
 from django.forms import NumberInput
 from django.conf import settings
-from products.models import Product 
+from products.models import Product
 
 
 class GetOrders(LoginRequiredMixin, ListView):
 		"""Show the Users Orders in Table"""
+
 		login_url = settings.LOGIN_URL
 
 		template_name = "orders/orders.html"
@@ -30,10 +32,11 @@ class GetOrders(LoginRequiredMixin, ListView):
 				order_id = request.POST.get("order_id")
 				order: Order = Order.objects.get(id=order_id)
 				order.delete()
+				messages.success(request , "Order Deleted Successfully!")
 				return redirect("orders:orders")
 
 
-# TODO there is a better way 
+# TODO there is a better way
 # TODO Put the Form in the Forms File
 # This mustn't be here because he must create the order from The Product itself
 class CreateOrder(LoginRequiredMixin, TemplateView):
@@ -49,14 +52,13 @@ class CreateOrder(LoginRequiredMixin, TemplateView):
 		Form = modelform_factory(Order, fields=fields, labels=labels, widgets=widgets)
 
 		def post(self, request):
-				print("post")
 				form = self.Form(request.POST)
 				try:
 						if form.is_valid():
 								order = form.save(False)
 								order.customer = request.user
 								order.save()
-								print("userCreated")
+								messages.success(request, "Order Created Successfully!")
 								return redirect(self.success_url)
 				except:
 						return Http404("Some Thing Went Wrong")
@@ -72,8 +74,10 @@ class CreateOrder(LoginRequiredMixin, TemplateView):
 				context[self.context_object_name] = self.form()
 				return context
 
+
 class UpdateOrder(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 		"""Update Order"""
+
 		login_url = settings.LOGIN_URL
 		model = Order
 		context_object_name = "form"
@@ -84,3 +88,7 @@ class UpdateOrder(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 		def test_func(self) -> Optional[bool]:
 				object: Order = self.get_object()
 				return self.request.user == object.customer
+
+		def form_valid(self, form) -> HttpResponse:
+				messages.success(self.request, "Order Updated Successfully!")
+				return super().form_valid(form)
